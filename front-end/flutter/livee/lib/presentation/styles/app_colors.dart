9af28ab8ee6f12f61545198942e96f7aac392b23
leehwi <dlgnl117@gmail.com>
2025-09-15 11:56:45 +0900

    250915 > front-end > flutter > 앱 색상 관리 시스템 도입 및 스타일 폴더 구조화

---
+++ front-end/flutter/livee/lib/presentation/styles/app_colors.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+// 색상 팔레트를 정의하는 클래스
+class AppColors {
+  // 이 클래스는 인스턴스화(객체 생성)할 필요가 없으므로 private 생성자를 만들기
+  AppColors._();
+
+  // 기본 색상 (Primary Color)
+  static const Color primary = Color(0xFF687CF4);
+
+  // [상수] 자주 사용되는 다른 색상들도 미리 정의해두면 좋습니다.
+  static const Color background = Color(0xFFF7F8FA);
+  static const Color white = Colors.white;
+  static const Color black = Colors.black;
+
+  // 텍스트 색상
+  static const Color textBlack = Color(0xFF1F2937);
+  static const Color textGrey = Color(0xFF6B7280);
+
+  // 비활성화 또는 보조적인 UI에 사용되는 색상
+  static const Color disabled = Color(0xFFE5E7EB);
+  static const Color border = Color(0xFFE5E7EB);
+
+  // 기타 색상
+  static const Color error = Colors.red;
+  static const Color success = Colors.green;
+}
