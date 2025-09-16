d65cfbfc40ec03fe542255b7eacf4cacf932d690
hwicoding <dlgnl117@gmail.com>
2025-09-17 00:37:04 +0900

    250917 > front-end > flutter > 자동배포 로직 수정 및 웹 타이틀 변경 테스트

--- front-end/flutter/livee/lib/main.dart
+++ front-end/flutter/livee/lib/main.dart
@@ -46,7 +46,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-      title: 'Livee Hello',
+      title: 'Livee best',
       theme: ThemeData(
         fontFamily: 'SUIT',
         scaffoldBackgroundColor: Colors.white,
